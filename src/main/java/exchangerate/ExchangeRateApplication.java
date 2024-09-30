f591f9a314e9e6f19114663e6a56c13536350821
HyungUk Ryu <hw0603@naver.com>
2024-09-30 23:59:04 +0900

    refactor(ExchangeRateApplication): ApplicationContext에서 빈을 찾아오도록 변경

--- src/main/java/exchangerate/ExchangeRateApplication.java
+++ src/main/java/exchangerate/ExchangeRateApplication.java
@@ -1,10 +1,16 @@
 package exchangerate;
 
 import exchangerate.decoupled.ExchangeRateRenderer;
+import exchangerate.decoupled.StandardInputExchangeRateProvider;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ExchangeRateApplication {
+
     public static void main(String[] args) {
-        final ExchangeRateRenderer renderer = null;
+        final ApplicationContext context = new ClassPathXmlApplicationContext("exchange-rate-context.xml");
+        final ExchangeRateRenderer renderer = context.getBean("exchangeRateRenderer", ExchangeRateRenderer.class);
+        renderer.setExchangeRateProvider(new StandardInputExchangeRateProvider());
         renderer.render();
     }
 }
